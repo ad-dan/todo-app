3afb097bb938cef73487885f9497d45a16bf6e15
Ad <adunreal@gmail.com>
2017-09-13 19:16:32 +0530

    Added better styling

--- scripts.js
+++ scripts.js
@@ -1,27 +1,7 @@
 $('document').ready(function(){
     setInterval(updatePage, 5000);
-    $('#submit').on('click',()=>{
-        const task = $('#todo').val().trim();
-        $.ajax({
-            url: '/submit',
-            data: {'task':task},
-            method: 'POST',
-            success: function(){
-                console.log('Sent');
-                $('#todo').val('');
-            }
-        });
-        console.log(task);
-    });
-    $('li').on('click',function(){
-        const task = $(this).text().trim().replace(/ /g,'-');
-        console.log(task);
-        $.ajax({
-            url: `/delete/${task}`,
-            method: 'delete',
-            success: updatePage
-        })
-    });
+    $('#submit').on('click',sendData);
+    $('li').on('click',bindEvents);
 });
 
 function updatePage(){
@@ -29,17 +9,34 @@ function updatePage(){
         $('#tasks').html(' ');
         data.forEach(todo=>{
             $('#tasks').append(`<li>${todo.task}</li>`);
-            $('li').on('click',function(){
-                const task = $(this).text().trim().replace(/ /g,'-');
-                console.log(task);
-                $.ajax({
-                    url: `/delete/${task}`,
-                    method: 'delete',
-                    success: function(){
-                        console.log('deleted data');
-                    }
-                })
-            });
+            $('li').on('click',bindEvents);
         });
     });
+}
+
+function bindEvents(){
+    const task = $(this).text().trim().replace(/ /g,'-');
+    $(this).remove();
+    console.log(task);
+    $.ajax({
+        url: `/delete/${task}`,
+        method: 'delete',
+        success: function(){
+            console.log('deleted data');
+        }
+    });
+}
+
+function sendData(){
+    const task = $('#todo').val().trim();
+    $.ajax({
+        url: '/submit',
+        data: {'task':task},
+        method: 'POST',
+        success: function(){
+            console.log('Sent');
+            $('#todo').val('');
+        }
+    });
+    console.log(task);
 }
